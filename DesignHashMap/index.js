bd05905fcc49b374db9d1db2c491977d660abad7
折田幸夫 <3204704@gmail.com>
2020-10-12 22:44:49 +0900

    途中まで回答

--- DesignHashMap/index.js
+++ DesignHashMap/index.js
@@ -17,3 +17,52 @@
 // hashMap.get（2）; // 1を返します
 // hashMap.remove（2）; // 2のマッピングを削除します
 // hashMap.get（2）; // -1を返します（見つかりません）
+
+/**
+ * Initialize your data structure here.
+ */
+var MyHashMap = function() {
+    this.set = [];
+};
+
+/**
+ * value will always be non-negative. 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+MyHashMap.prototype.put = function(key, value) {
+      if (this.set.indexOf(key) === -1) {
+          this.set.push({"key":key,"value":value);
+};
+
+/**
+ * Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key 
+ * @param {number} key
+ * @return {number}
+ */
+MyHashMap.prototype.get = function(key) {
+    
+};
+
+/**
+ * Removes the mapping of the specified value key if this map contains a mapping for the key 
+ * @param {number} key
+ * @return {void}
+ */
+MyHashMap.prototype.remove = function(key) {
+　　//仮にkeyが3の場合そのオブジェクトを削除
+    const key = this.set.indexOf(key);
+    this.set.some((value,index)=>{
+        if(value.id===key)this.set.splice(index,1)
+    })
+});
+};
+
+/** 
+ * Your MyHashMap object will be instantiated and called as such:
+ * var obj = new MyHashMap()
+ * obj.put(key,value)
+ * var param_2 = obj.get(key)
+ * obj.remove(key)
+ */
